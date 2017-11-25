82fe88108c30769beeeb33e91c53fb9a1f67bb42
Torstein <torsteinv64@gmail.com>
2017-11-26 00:37:14 +0100

    Implemented error messages in testing lib

--- src/test/lib/testing.cpp
+++ src/test/lib/testing.cpp
@@ -8,12 +8,14 @@ struct {
     string testname;
     bool passing;
     bool global_passing;
+    string error_msg;
 } TestState;
 
 void starttest(string name)
 {
     TestState.testname = name;
     TestState.passing = true;
+    TestState.error_msg = "";
 }
 
 void stoptest()
@@ -22,6 +24,7 @@ void stoptest()
         cout << " - " << TestState.testname << ": succeeded!" << endl;
     } else {
         cout << " - " << TestState.testname << ": failed!" << endl;
+        cout << "Error: " << TestState.error_msg << endl;
     }
 }
 
@@ -35,5 +38,6 @@ void assert(bool assertion, string msg)
     if(!assertion) {
         TestState.passing = false;
         TestState.global_passing = false;
+        TestState.error_msg += "\n" + msg;
     }
 }
