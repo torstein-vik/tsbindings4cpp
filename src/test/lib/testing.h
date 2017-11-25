afd23e23def59954fdb3e3a9d2874339abed64a0
Torstein <torsteinv64@gmail.com>
2017-11-26 00:31:10 +0100

    Added headers for basic test-suite functions

--- src/test/lib/testing.h
+++ src/test/lib/testing.h
@@ -1,5 +1,11 @@
 #ifndef __TANNAKIANSYMBOL_H_INCLUDED
 #define __TANNAKIANSYMBOL_H_INCLUDED
 
+#include <string>
+
+void starttest(std::string name);
+void stoptest();
+int  testsfinished();
+void assert(bool assertion, std::string msg);
 
 #endif
