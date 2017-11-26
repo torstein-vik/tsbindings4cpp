7df27c40c3d58f6a159092d4666f12f681ca1eed
Torstein <torsteinv64@gmail.com>
2017-11-26 01:38:09 +0100

    Added tannakiansymbol class to header

--- src/main/tannakiansymbol.h
+++ src/main/tannakiansymbol.h
@@ -1,5 +1,17 @@
 #ifndef __TANNAKIANSYMBOL_H_INCLUDED
 #define __TANNAKIANSYMBOL_H_INCLUDED
 
+#include <vector>
+#include <tuple>
+
+
+template <class T>
+class tannakiansymbol {
+    private:
+        std::vector<std::pair<T, int> > data;
+
+    public:
+        tannakiansymbol (std::vector<std::pair<T, int> > multiplicities);
+};
 
 #endif
